a9fb5a46c135119c060c84cec27f59a59309ded4
Elaine Gibson <ypsvlq@gmail.com>
2023-10-02 22:40:04 +0100

    config: improve error logging

--- src/config.zig
+++ src/config.zig
@@ -3,7 +3,6 @@ const Ini = @import("Ini.zig");
 const vfs = @import("vfs.zig");
 const game = @import("game.zig");
 const input = @import("input.zig");
-const log = std.log.scoped(.config);
 
 pub var width: u16 = 640;
 pub var height: u16 = 480;
@@ -25,32 +24,34 @@ fn set(comptime T: type, ptr: anytype, value: []const u8) !void {
     }
 }
 
-pub fn load() !void {
-    const bytes = vfs.readFile(game.temp_allocator, "config.ini") catch return;
-    var iter = Ini{ .bytes = bytes };
-    while (iter.next() catch |err| {
-        log.err("config.ini line {}: {s}", .{ iter.line, @errorName(err) });
-        return error.InvalidConfig;
-    }) |entry| {
+fn process(iter: *Ini) !void {
+    next: while (try iter.next()) |entry| {
         if (iter.section.len == 0) {
             inline for (@typeInfo(@This()).Struct.decls) |decl| {
                 if (std.mem.eql(u8, decl.name, entry.key)) {
                     const ptr = &@field(@This(), decl.name);
                     const T = @TypeOf(ptr.*);
                     try set(T, ptr, entry.value);
+                    continue :next;
                 }
             }
+            return error.UnknownKey;
         } else {
             const section_handlers = std.ComptimeStringMap(*const fn ([]const u8, []const u8) anyerror!void, .{
                 .{ "keys", input.keyConfig },
                 .{ "joystick", input.joystickConfig },
             });
-            if (section_handlers.get(iter.section)) |handler| {
-                try handler(entry.key, entry.value);
-            } else {
-                log.err("config.ini line {}: unknown section '{s}'", .{ iter.line, iter.section });
-                return error.InvalidConfig;
-            }
+            const handler = section_handlers.get(iter.section) orelse return error.UnknownSection;
+            try handler(entry.key, entry.value);
         }
     }
+}
+
+pub fn load() !void {
+    const bytes = vfs.readFile(game.temp_allocator, "config.ini") catch return;
+    var iter = Ini{ .bytes = bytes };
+    process(&iter) catch |err| {
+        std.log.err("config.ini line {}: {s}", .{ iter.line, @errorName(err) });
+        return err;
+    };
 }
