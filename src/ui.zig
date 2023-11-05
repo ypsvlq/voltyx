390200a7ac0b77998f3ae46be0f465b3854ff580
Elaine Gibson <ypsvlq@gmail.com>
2023-11-05 11:16:40 +0000

    ui: rename rgb to image

--- src/ui.zig
+++ src/ui.zig
@@ -8,7 +8,7 @@ const renderer = @import("renderer.zig");
 
 pub var scale: f32 = 1;
 
-var rgb_program: glw.Program(.{
+var image_program: glw.Program(.{
     .Attrib = enum { vertex },
     .Uniform = enum { projection, texture },
 }) = undefined;
@@ -17,8 +17,8 @@ pub fn init() !void {
     scale = game.window.getContentScale().y_scale;
     game.window.setContentScaleCallback(scaleCallback);
 
-    try rgb_program.compile("shaders/rgb.vert", "shaders/rgb.frag");
-    rgb_program.enableAttribArray(.vertex);
+    try image_program.compile("shaders/image.vert", "shaders/image.frag");
+    image_program.enableAttribArray(.vertex);
 }
 
 fn scaleCallback(_: glfw.Window, _: f32, y_scale: f32) void {
@@ -39,17 +39,17 @@ pub fn setTextSize(size: u32) !void {
 }
 
 pub fn drawImage(texture: u32, x_: u16, y_: u16, w_: u16, h_: u16) void {
-    rgb_program.use();
-    rgb_program.setUniform(.projection, &renderer.ortho);
+    image_program.use();
+    image_program.setUniform(.projection, &renderer.ortho);
 
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    rgb_program.setUniform(.texture, 0);
+    image_program.setUniform(.texture, 0);
 
     const x: f32 = @floatFromInt(x_);
     const y: f32 = @floatFromInt(y_);
     const w: f32 = @floatFromInt(w_);
     const h: f32 = @floatFromInt(h_);
 
-    renderer.drawQuad(rgb_program, x, renderer.height - y - h, w, h);
+    renderer.drawQuad(image_program, x, renderer.height - y - h, w, h);
 }
